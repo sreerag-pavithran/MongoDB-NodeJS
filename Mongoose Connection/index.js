944f0ace5edf470815fecc30c3835966a2edfce0
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-05-18 23:12:52 +0530

    Mongoose Installation

---
+++ Mongoose Connection/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "React JS Course",
+    author: "Sandra",
+    tags: ["node", "frontend"],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+};
+
+createCourse();
